d3cefa370131a90727f88a61e622a29dfafb1f3d
Cristi Sandu <cristisandu@csx.ro>
2023-03-30 00:31:55 +0300

    Initial Commit

---
+++ Poli Iasi Arena/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Poli Iasi Arena
+//
+//  Created by Cristi Sandu on 30.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Poli Iasi Arena/Poli_Iasi_ArenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poli_Iasi_ArenaApp.swift
+//  Poli Iasi Arena
+//
+//  Created by Cristi Sandu on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Poli_Iasi_ArenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
