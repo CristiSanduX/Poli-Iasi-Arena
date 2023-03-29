ca7d3e93af8866d4b903a949f8a248da12aeb90f
Cristi Sandu <cristisandu@csx.ro>
2023-03-30 01:03:32 +0300

    Splash screen

--- Poli Iasi Arena/Poli_Iasi_ArenaApp.swift
+++ Poli Iasi Arena/Poli_Iasi_ArenaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Poli_Iasi_ArenaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
---
+++ Poli Iasi Arena/SplashScreenView.swift
@@ -0,0 +1,59 @@
+//
+//  SplashScreenView.swift
+//  Poli Iasi Arena
+//
+//  Created by Cristi Sandu on 30.03.2023.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    
+    @State private var isActive = false
+    @State private var size = 0.7
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        
+        if isActive {
+            ContentView()
+        }
+        else {
+            VStack {
+                VStack {
+                    Image("logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200,alignment: .center)
+                    Text("Poli Iași Arena")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 1.0
+                        self.opacity = 1.0
+                    }
+                    
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
