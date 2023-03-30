c11496580921599da3a21d42a010490de2a7f853
Cristi Sandu <cristisandu@csx.ro>
2023-03-30 11:05:50 +0300

    SignUp Screen

--- Poli Iasi Arena/AuthViews/SignUpView.swift
+++ Poli Iasi Arena/AuthViews/SignUpView.swift
@@ -1,5 +1,5 @@
 //
-//  LoginView.swift
+//  SignUpView.swift
 //  Poli Iasi Arena
 //
 //  Created by Cristi Sandu on 30.03.2023.
@@ -7,20 +7,19 @@
 
 import SwiftUI
 
-struct LoginView: View {
-    
+struct SignUpView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     
     var body: some View {
         ZStack {
-            Color.white.edgesIgnoringSafeArea(.all)
+            Color.blue.edgesIgnoringSafeArea(.all)
             
             VStack {
                 Text("Poli Iași Arena")
                     .font(.largeTitle)
                     .bold()
-                    .foregroundColor(.blue)
+                    .foregroundColor(.black)
                     .padding()
                 Spacer()
                 
@@ -75,23 +74,23 @@ struct LoginView: View {
                 
                 
                 Button(action: {}) {
-                    Text("Nu ai cont?")
-                        .foregroundColor(.blue.opacity(0.7))
+                    Text("Ai deja cont?")
+                        .foregroundColor(.black.opacity(0.7))
                 }
                 Spacer()
                 
                 Button{
                     
                 } label: {
-                    Text("Autentifică-te")
-                        .foregroundColor(.black)
+                    Text("Creează cont nou")
+                        .foregroundColor(.white)
                         .font(.title3)
                         .bold()
                         .frame(maxWidth: .infinity)
                         .padding()
                         .background(
                             RoundedRectangle(cornerRadius: 10)
-                                .fill(Color.blue)
+                                .fill(Color.black)
                                 .padding(.horizontal)
                         )
                 }
@@ -101,8 +100,8 @@ struct LoginView: View {
     }
 }
 
-struct LoginView_Previews: PreviewProvider {
+struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        SignUpView()
     }
 }
